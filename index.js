c36beb0ca466cdd65f2f2a7911c430e9d98e3ba5
santibout <santibout@yahoo.com>
2021-06-15 13:38:52 -0700

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const sgMail = require("@sendgrid/mail");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 require("dotenv").config();
 
@@ -8,6 +9,7 @@ const app = express();
 
 sgMail.setApiKey(process.env.SENDGRID_MAIL_SERVER_KEY);
 
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
