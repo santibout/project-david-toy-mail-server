ec4cb65fc57c7662c9457c8e0bf38c4d1f391774
santibout <santibout@yahoo.com>
2021-06-18 10:59:19 -0700

    used object template and modified email to be sent dynamically

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 const app = express();
 
 sgMail.setApiKey(process.env.SENDGRID_MAIL_SERVER_KEY);
+console.log(process.env.SENDGRID_MAIL_SERVER_KEY);
 
 // app.use(cors());
 app.use(function (req, res, next) {
@@ -20,30 +21,46 @@ app.use(function (req, res, next) {
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  console.log("hi");
-  res.send("this is the home root");
-});
-
+// app.get("/", (req, res) => {
+//   console.log("hi");
+//   res.send("this is the home root");
+// });
+// body.map((x) => {
+//   for (let d in x) {
+//     if (d === "img") htmlStr += `<img src=${x[d]} />`;
+//     htmlStr += `<p>${d}: ${x[d]}</p>`;
+//   }
+//   htmlStr += "<br />";
+// });
+generateHtmlString(body) {
+  return `
+    <h2>Hi, ${body.email}</h2>
+    <p>is amazon fresh customer: ${body.isAmazonFresh}</p>
+    <a src=${body.amazonURL}>${body.amazonURL}</a>
+    <p>Is amazon prime customer: ${body.isAmazonPrime}</p>
+    <p>Is selected: ${body.isSelected}</p>
+    <p>title: ${body.title}</p>
+    <p>Image: ${body.fileUrlPath}</p>
+    <img src=${body.fileUrlPath} />
+  `  
+}
 app.post("/send", (req, res) => {
   console.log(req.body);
-  // const { img } = req.body;
+  const {
+    email,
+    isAmazonFresh,
+    amazonURL,
+    isAmazonPrime,
+    isSelected,
+    title,
+    fileUrlPath,
+  } = req.body;
   let htmlStr = "";
-  req.body.map((x) => {
-    for (let d in x) {
-      if (d === "img") htmlStr += `<img src=${x[d]} />`;
-      htmlStr += `<p>${d}: ${x[d]}</p>`;
-    }
-    htmlStr += "<br />";
-  });
+  const htmlStr = generateHtmlString(req.body);
   console.log(htmlStr);
   const msg = {
     from: "samuel.santibout@gmail.com",
-    to: [
-      "santibout@yahoo.com",
-      // "david@kayoventures.com",
-      "samuel.santibout@gmail.com",
-    ],
+    to: ["santibout@yahoo.com", "david@kayoventures.com", `${email}`],
     subject: "Mail Server",
     html: htmlStr,
   };
